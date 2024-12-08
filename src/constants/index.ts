c281cf0ab4b67083fd80c8c1958a72f0f67ac6d7
danimkim <danimkim.dev@gmail.com>
2024-12-08 22:53:42 +0900

    initial commit

---
+++ src/constants/index.ts
@@ -0,0 +1,1 @@
+export const API_URL = `http://www.potenfinance.site:8080/mcount`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,33 @@
-import Head from "next/head";
-import Image from "next/image";
-import localFont from "next/font/local";
-import styles from "@/styles/Home.module.css";
+import Head from 'next/head';
+import Image from 'next/image';
+import localFont from 'next/font/local';
+import styles from '@/styles/Home.module.css';
+import axios, { AxiosResponse } from 'axios';
+import { useState } from 'react';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
 export default function Home() {
+  const [result, setResult] = useState<number>();
+
+  const handleClick = async () => {
+    const res: AxiosResponse<{ resultInt: number }> = await axios.get<{
+      resultInt: number;
+    }>(`/mcount`);
+    setResult(res.data.resultInt);
+  };
+
+  const onReset = () => setResult(undefined);
+
   return (
     <>
       <Head>
@@ -35,83 +48,22 @@ export default function Home() {
             height={38}
             priority
           />
-          <ol>
+          <ul>
             <li>
-              Get started by editing <code>src/pages/index.tsx</code>.
+              {`Click the 'Send Request' Button and check the request result below.`}
             </li>
-            <li>Save and see your changes instantly.</li>
-          </ol>
+            <li>Request Result: {result}</li>
+          </ul>
 
           <div className={styles.ctas}>
-            <a
-              className={styles.primary}
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className={styles.logo}
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
+            <a className={styles.primary} onClick={handleClick}>
+              Send Request
             </a>
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.secondary}
-            >
-              Read our docs
+            <a className={styles.secondary} onClick={onReset}>
+              Reset
             </a>
           </div>
         </main>
-        <footer className={styles.footer}>
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            Learn
-          </a>
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
-        </footer>
       </div>
     </>
   );
